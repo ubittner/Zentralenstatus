fcac65745cbeca8aecdbe765e88e576940eaafaf
ubittner <ubittner@users.noreply.github.com>
2025-12-17 04:50:15 +0100

    deleted

--- Zentralenstatus/helper/ZENS_autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-foreach (glob(__DIR__ . '/*.php') as $filename) {
-    if (basename($filename) != 'ZENS_autoload.php') {
-        include_once $filename;
-    }
-}
